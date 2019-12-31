34c4f52c01e7a9e0116f5628bffe99f6b30e2492
Aleksandr Koltovich <avkoltovich@gmail.com>
2019-12-31 03:16:01 +0300

    Стилизует index

--- source/js/script.js
+++ source/js/script.js
@@ -10,3 +10,31 @@ if ($menu) {
     $menu.classList.toggle('header__menu--closed');
   });
 }
+
+// Yandex карта
+
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+      center: [59.936397, 30.321166],
+      zoom: 16,
+      controls: []
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
+
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
+
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      hintContent: 'PINK',
+    }, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/icon-map-marker.svg',
+      iconImageSize: [36, 36],
+      iconImageOffset: [0, 0]
+    });
+
+  myMap.geoObjects
+    .add(myPlacemark);
+});
